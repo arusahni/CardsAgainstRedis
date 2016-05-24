bc869c689f7af2c6405dfa12539cd6ca9a102ac0
Aru Sahni <arusahni@gmail.com>
2016-05-23 21:14:31 -0400

    Scan Redis for cards.

--- client.py
+++ client.py
@@ -24,32 +24,38 @@ def get_connection(host="10.28.59.172", port=6379, db=0):
 class CarGame:
     """Play the game"""
 
-    def __init__(self, conn):
+    def __init__(self, conn, user):
         """Do the thing """
         self.hand = []
         self.score = 0
         self._conn = conn
+        self.user = user
 
     def get_hand(self):
         if len(self.hand) < HAND_SIZE:
-            self._draw_up(HAND_SIZE - len(self.hand))
+            self._draw_up()
         print("Hand has {} cards".format(len(self.hand)))
 
-    def _draw_up(self, number):
-        for _ in range(number):
-            card_text = random.choice(white_cards)
-            card_info = self._conn.get("white:{}".format(card_text))
-            card_info = card_info.decode('utf-8')
-            if card_info: #taken
-                print("Taken: {}".format(card_text))
-            else:
+    def _draw_up(self):
+        for card_text in white_cards:
+            card_user = self._conn.get("white:{}".format(card_text))
+            card_user = card_user.decode('utf-8')
+            if card_user == self.user:
+                print("Taking: {}".format(card_text))
                 self.hand.append(card_text)
-                print("Not taken: {}".format(card_text))
+            else:
+                print("Not taking: {}".format(card_text))
+            if len(self.hand) >= HAND_SIZE:
+                break
+        if len(self.hand) < HAND_SIZE:
+            print("!! Deck is empty.")
 
 def play_game():
     conn = get_connection()
-    game = CarGame(conn)
+    user = input("User name: ")
+    game = CarGame(conn, user)
     game.get_hand()
+    print("Bye, {}".format(game.user))
 
 if __name__ == "__main__":
     play_game()
